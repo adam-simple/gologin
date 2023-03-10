1c876472347b86f4810736d39479094e734a9bbc
Adam Scrivener <adamscrivener@Adams-Air.fios-router.home>
2023-03-10 16:57:05 -0500

    tostring

--- src/gologin.js
+++ src/gologin.js
@@ -890,7 +890,7 @@ export class GoLogin {
       }
 
       console.log(params);
-      const command = str(ORBITA_BROWSER).join(params.join(' '), ' ')
+      const command = ORBITA_BROWSER.toString().join(params.join(' '), ' ')
       logger('SPAWN CMD', command);
       return command
     }
