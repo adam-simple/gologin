e6db2c42325e8585675752c15c34dbba65f484f2
Adam Scrivener <adamscrivener@Adams-Air.fios-router.home>
2023-03-10 12:32:51 -0500

    more

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-DISPLAY=:$5 $1 --no-sandbox --remote-debugging-port=$2 --proxy-server=$3 --user-data-dir=$4 --tz=$6 --load-extension=$7 --password-store=basic --lang=en --new-window
+DISPLAY=:$4 $1 --no-sandbox --proxy-server=$2 --user-data-dir=$3 --tz=$5 --load-extension=$6 --password-store=basic --lang=en --new-window
