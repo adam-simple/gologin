c2370f016f04f46d90118b45288748d679a3e23a
Adam Scrivener <adamscrivener@Adams-Air.fios-router.home>
2023-03-11 10:20:48 -0500

    enable logging

--- src/gologin.js
+++ src/gologin.js
@@ -822,7 +822,7 @@ export class GoLogin {
 
     if (this.vnc_port) {
       const script_path = _resolve(__dirname, '../run.sh');
-      let command = `DISPLAY=:${this.vnc_port} ${ORBITA_BROWSER} --disable-gpu --v=4 --no-sandbox --remote-debugging-port=${remote_debugging_port} --proxy-server=${proxy} --user-data-dir=${profile_path} --tz=${tz} --password-store=basic --lang=en --new-window`
+      let command = `DISPLAY=:${this.vnc_port} ${ORBITA_BROWSER} --enable-logging --disable-gpu --v=4 --no-sandbox --remote-debugging-port=${remote_debugging_port} --proxy-server=${proxy} --user-data-dir=${profile_path} --tz=${tz} --password-store=basic --lang=en --new-window`
       logger('ENV', { env })
       for (const key in env) {
         if (env.hasOwnProperty(key)) {
